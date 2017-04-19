7cb15d1838765d8167010ff180cf8f5144a2249f
peikovakate <peikovakate@gmail.com>
2017-04-19 14:43:37 +0300

    mvp

---
+++ TestTask/DComparer.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace TestTask
+{
+    //logic of comparing and storing information in directories
+    class DComparer
+    {
+        private string path1;
+
+        public string Path1
+        {
+            set
+            {
+                path1 = value;
+                reload();
+            }
+
+            get
+            {
+                return path1;
+            }
+        }
+
+        private string path2;
+
+        public string Path2
+        {
+            set
+            {
+                path2 = value;
+                reload();
+            }
+
+            get
+            {
+                return path2;
+            }
+        }
+
+        public List<FInfo> Files { get; }
+
+        public DComparer()
+        {
+            Files = new List<FInfo>();
+        }
+
+        //refresh information about directories
+        private void reload()
+        {
+            Files.Clear();
+
+            if (path1 != null)
+            {
+                checkFolder(path1);
+            }
+
+            if (path2 != null)
+            {
+                checkFolder(path2);
+            }
+
+        }
+
+        private void checkFolder(string path)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            FileInfo[] info = dirInfo.GetFiles("*.*");
+
+            foreach (FileInfo inf in info)
+            {
+                var fInf = new FInfo();
+                fInf.Name = inf.Name;
+                fInf.Size = inf.Length;
+                fInf.Date = inf.LastWriteTime;
+                var fileInf = Files.Find(f => f.Name == inf.Name);
+                if (fileInf != null)
+                {
+                    if (fileInf.Size == inf.Length)
+                    {
+                        fileInf.State = state.SameSize;
+                        fInf.State = state.SameSize;
+                    }
+                    else
+                    {
+                        fileInf.State = state.DifSize;
+                        fInf.State = state.DifSize;
+                    }
+                }
+                else
+                {
+                    fInf.State = path == path1 ? state.First : state.Second;
+                }
+                Files.Add(fInf);
+            }
+        }
+    }
+}
---
+++ TestTask/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+
+namespace TestTask
+{
+    public partial class MainWindow : Window
+    {
+        DComparer comparer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            comparer = new DComparer();
+        }
+
+        private void reloadTable()
+        {
+            InfoTable.ItemsSource = null;
+            InfoTable.ItemsSource = comparer.Files;
+        }
+
+        private void FirstDirectory_Change(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    comparer.Path1 = dialog.SelectedPath;
+                    FirstPathTextBlock.Text = dialog.SelectedPath;
+                    reloadTable();
+                }
+            }   
+        }
+
+        private void SecondDirectory_Change(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    comparer.Path2 = dialog.SelectedPath;
+                    SecondPathTextBlock.Text = dialog.SelectedPath;
+                }
+            }
+            reloadTable();
+        }
+    }
+}
